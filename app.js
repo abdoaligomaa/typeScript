27cbc7cfca3c43ca1cef34e11a5fd125f49cdc7a
abdoaligomaa <abdoaligomaa107@gmail.com>
2022-05-30 02:52:25 +0200

    typies in ts

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hi from ts file');
+function add(n1, n2) {
+    return n1 + n2;
+}
+// let a="10";// you can not do that with type script and the you can not compile this file to js
+var a = 10;
+var b = 10;
+var result = add(a, b);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('hi from ts file')
+
+function add(n1:number,n2:number){
+    return n1+n2;
+}
+// let a="10";// you can not do that with type script and the you can not compile this file to js
+let a=10;
+let b=10;
+const result =add(a,b)
+console.log(result)
